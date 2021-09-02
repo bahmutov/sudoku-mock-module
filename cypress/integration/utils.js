811132b7ffd9a3c9d34215e03ec9e152496ce77c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-02 09:16:34 -0400

    move inline into separate spec

--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import initialBoard from '../fixtures/init-array.json'
+
 export const replaceWebPackModule = (
   fileName,
   newModuleDefault,
@@ -53,5 +55,18 @@ export const replaceWebPackModule = (
           || ` +
         finish
     })
+  })
+}
+
+/**
+ * Confirms the first cells in the game are filled
+ * with the initial values from the mock board array.
+ */
+export const checkBoardMocked = () => {
+  cy.get('.game__cell').should('have.length', initialBoard.length)
+  // take the first N items from the array
+  // and check the cells on the board
+  Cypress._.slice(initialBoard, 0, 3).forEach((value, index) => {
+    cy.get('.game__cell').eq(index).should('have.text', value)
   })
 }
